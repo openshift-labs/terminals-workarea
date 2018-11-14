328e7cf47fbf14b9496a8ce529173b242a822b36
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-11-14 16:42:18 +1100

    Environment variable workaround no longer required.

--- terminal/bin/start-terminal.sh
+++ terminal/bin/start-terminal.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-set -a
-. /opt/workshop/etc/envvars
-set +a
-
 export SHELL=/bin/bash
 
 if [ x"$JUPYTERHUB_USER" != x"" ]; then
@@ -11,17 +7,5 @@ if [ x"$JUPYTERHUB_USER" != x"" ]; then
 else
     export PS1="[\w] $ "
 fi
-
-unset JUPYTERHUB_ADMIN_ACCESS
-unset JUPYTERHUB_BASE_URL
-unset JUPYTERHUB_CLIENT_ID
-unset JUPYTERHUB_API_TOKEN
-unset JUPYTERHUB_API_URL
-unset JUPYTERHUB_USER
-unset JUPYTER_IMAGE_SPEC
-unset JUPYTERHUB_OAUTH_CALLBACK_URL
-unset JUPYTER_IMAGE
-unset JUPYTERHUB_HOST
-unset JUPYTERHUB_SERVICE_PREFIX
 
 exec /bin/bash "$@"
