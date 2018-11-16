bb77d2f15338d419c248e404f4f7a24993b0bf48
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-11-16 16:59:08 +1100

    Swap to using express for proxy.

--- terminal/proxy.js
+++ terminal/proxy.js
@@ -1,73 +1,33 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
-    url = require('url');
+var express = require('express');
+var proxy = require('http-proxy-middleware');
+var basic_auth = require('express-basic-auth')
 
-var uri_root_path = process.env.URI_ROOT_PATH || ''
+var app = express();
+
+var uri_root_path = process.env.URI_ROOT_PATH || '';
 
 var terminal_app = 'http://127.0.0.1:8081';
-var terminal_root_url = '^' + uri_root_path + '/?$'
 
 var auth_username = process.env.AUTH_USERNAME;
 var auth_password = process.env.AUTH_PASSWORD;
 
-var proxy = httpProxy.createProxyServer();
-
-function on_error(err, req, res) {
-  res.writeHead(500, {
-    'Content-Type': 'text/plain'
-  });
-
-  res.end('Something went wrong.');
+if (auth_username) {
+    app.use(basic_auth({
+        challenge: true,
+        realm: 'Terminal',
+        authorizer: function (username, password) {
+            return username == auth_username && password == auth_password;
+        }
+    }));
 }
 
-var server = http.createServer(function(req, res) {
-  var auth = req.headers['authorization'];
+app.get('^' + uri_root_path + '/?$', function (req, res) {
+    res.redirect(uri_root_path + '/session/1');
+})
 
-  if (auth_username) {
-    if (!auth) {
-	res.statusCode = 401;
-	res.setHeader('WWW-Authenticate', 'Basic realm="Terminal"');
+app.use(uri_root_path, proxy({
+    target: terminal_app,
+    ws: true
+}));
 
-	res.end('Login required.');
-
-        return;
-    }
-    else {
-      var tmp = auth.split(' ');
-      var buf = new Buffer(tmp[1], 'base64');
-      var plain_auth = buf.toString();
-
-      var creds = plain_auth.split(':');
-      var username = creds[0];
-      var password = creds[1];
-
-      if ((username != auth_username) || (password != auth_password)) {
-	res.statusCode = 401;
-	res.setHeader('WWW-Authenticate', 'Basic realm="Terminal"');
-
-	res.end('Access denied.');
-
-        return;
-      }
-    }
-  }
-
-  var parsed_url = url.parse(req.url);
-
-  if (parsed_url.pathname.match(terminal_root_url)) {
-    res.statusCode = 302;
-    res.setHeader('Location', uri_root_path + '/session/1');
-
-    res.end('Redirect to named session.');
-
-    return;
-  }
-
-  proxy.web(req, res, { target: terminal_app }, on_error);
-});
-
-server.on('upgrade', function (req, socket, head) {
-  proxy.ws(req, socket, head, { target: terminal_app }, on_error);
-});
-
-server.listen(8080, "0.0.0.0");
+app.listen(8080);
