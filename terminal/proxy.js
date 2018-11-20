3593dbef61c858bc40c480652e4f58c1e5ba904f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-11-20 14:13:42 +1100

    Restructure application code and use sub app for terminal.

--- terminal/proxy.js
+++ terminal/proxy.js
@@ -6,14 +6,17 @@ var uuid = require('uuid');
 var http = require('http');
 var url = require('url');
 
+// Setup the root application. Everything will actually be under a
+// mount point corresponding to the specific user. This is added in
+// each of the routes when defined.
+
 var app = express();
 
 var uri_root_path = process.env.URI_ROOT_PATH || '';
 
-var terminal_app = 'http://127.0.0.1:8081';
-
-// For standalone container deployment, provide ability to add
-// authentication using HTTP Basic authentication.
+// For standalone container deployment, provide the ability to enable
+// authentication using HTTP Basic authentication. In this case there
+// will be no user object added to the client session.
 
 var auth_username = process.env.AUTH_USERNAME;
 var auth_password = process.env.AUTH_PASSWORD;
@@ -27,6 +30,29 @@ if (auth_username) {
         }
     }));
 }
+
+// Enable use of a client session for the user. This is used to track
+// whether the user has logged in when using oauth. The expiry for the
+// cookie is relatively short as the oauth handshake is hidden from the
+// user as it is only negoitating with JupyterHub itself. Having a short
+// timeout means that the user will be periodically checked as still
+// being allowed to access the instance. This is so that a change by a
+// JupyterHub admin to status of a user is detected early and they are
+// kicked out.
+
+app.use(session({
+    name: 'workshop-session-id',
+    genid: function(req) {
+        return uuid.v4()
+    },
+    secret: uuid.v4(),
+    cookie: {
+        path: uri_root_path,
+        maxAge: 60*1000
+    },
+    resave: false,
+    saveUninitialized: true
+}));
 
 // For JupyterHub, to ensure that only the user, or an admin can access
 // anything, we need to perform an oauth handshake with JupyterHub and
@@ -42,31 +68,6 @@ var jupyterhub_route = process.env.JUPYTERHUB_ROUTE
 var hostname = process.env.HOSTNAME;
 
 if (jupyterhub_client_id) {
-    // Enable use of a client session for the user. This is used to
-    // track whether the user has logged in using oauth. The expiry for
-    // the cookie is relatively short as the oauth handshake is hidden
-    // from the user as it is only negoitating with JupyterHub itself.
-    // Having a short timeout means that the user will be periodically
-    // checked as still being allowed to access the instance. This is so
-    // that a change by a JupyterHub admin to status of a user is
-    // detected early and they are kicked out.
-
-    app.use(session({
-        name: 'workshop-session-id',
-        genid: function(req) {
-            return uuid.v4()
-        },
-        secret: jupyterhub_api_token,
-        cookie: {
-            path: uri_root_path,
-            maxAge: 60*1000
-        },
-        resave: false,
-        saveUninitialized: true
-    }));
-
-    // Setup details for the oauth handshake with JupyterHub.
-
     var api_url = url.parse(jupyterhub_api_url);
 
     var credentials = {
@@ -100,7 +101,7 @@ if (jupyterhub_client_id) {
             var code = req.query.code;
             var state = req.query.state;
 
-            // This is retrieve the next URL to redirect to from the session
+            // This retrieves the next URL to redirect to from the session
             // for this particular oauth handshake.
 
             var next_url = req.session.handshakes[state];
@@ -116,34 +117,34 @@ if (jupyterhub_client_id) {
 
             var user_url = jupyterhub_api_url + '/user';
 
-	    var parsed_user_url = url.parse(user_url);
+            var parsed_user_url = url.parse(user_url);
 
-	    var user_url_options = {
-		host: parsed_user_url.hostname,
-		port: parsed_user_url.port,
-		path: parsed_user_url.path,
-		headers: {
-		    authorization: 'token ' + token_result.token.access_token
-		}
-	    };
+            var user_url_options = {
+                host: parsed_user_url.hostname,
+                port: parsed_user_url.port,
+                path: parsed_user_url.path,
+                headers: {
+                    authorization: 'token ' + token_result.token.access_token
+                }
+            };
 
             // This is the callback to fetch the user details from
             // JupyterHub so we can authorize that they have access.
 
-	    http.get(user_url_options, (user_res) => {
-		let data = '';
+            http.get(user_url_options, (user_res) => {
+                let data = '';
 
-		user_res.on('data', (chunk) => {
-		    data += chunk;
-		});
+                user_res.on('data', (chunk) => {
+                    data += chunk;
+                });
 
                 user_res.on('end', () => {
-		    user = JSON.parse(data);
+                    user = JSON.parse(data);
 
                     // The user who has logged in must be an admin or
                     // the user of the instance.
 
-		    if (!user.admin) {
+                    if (!user.admin) {
                         if (user.name != jupyterhub_user) {
                             return res.status(403).json('Access forbidden');
                         }
@@ -153,14 +154,14 @@ if (jupyterhub_client_id) {
 
                     console.log('Allowing access to', user);
 
-		    res.redirect(next_url);
+                    res.redirect(next_url);
 
                     return;
                 });
-	    }).on('error', (err) => {
-	        console.error('Error', err.message);
+            }).on('error', (err) => {
+                console.error('Error', err.message);
                 return res.status(500).json('Error occurred');
-	    });
+            });
 
             return;
         } catch(err) {
@@ -173,7 +174,7 @@ if (jupyterhub_client_id) {
     // here whenever any request arrives and user has not been verified
     // or when the user session has expired and need to revalidate.
 
-    app.get(uri_root_path + '/auth', (req, res) => {
+    app.get(uri_root_path + '/oauth_handshake', (req, res) => {
         // Stash the next URL after authentication in the user session
         // keyed by unique code for this oauth handshake. Use the code
         // as the state for oauth requests.
@@ -207,33 +208,40 @@ if (jupyterhub_client_id) {
 
         if (!req.session.user) {
             next_url = encodeURIComponent(req.url);
-	    res.redirect(uri_root_path + '/auth?next=' + next_url);
+            res.redirect(uri_root_path + '/oauth_handshake?next=' + next_url);
         }
-        else
+        else {
             next();
+        }
     })
 }
 
-// Redirecting to terminal at a sub path. This is to allow other
-// applications to be hosted behind this proxy if necessary.
+// By default redirect to terminal application. Have major components
+// mounted as sub URLs to allow other applications to be hosted behind
+// this application if necessary.
 
 app.get('^' + uri_root_path + '/?$', function (req, res) {
     res.redirect(uri_root_path + '/terminal');
 })
 
-// If no terminal session provided, redirect to session 1. This ensures
-// user always get the same session and not a new one each time if refresh
-// the web browser or access same URL from another browser window.
+// Setup proxying to terminal application. If no terminal session is
+// provided, redirect to session 1. This ensures user always get the
+// same session and not a new one each time if refresh the web browser
+// or access same URL from another browser window.
 
-app.get('^' + uri_root_path + '/terminal/?$', function (req, res) {
-    res.redirect(uri_root_path + '/terminal/session/1');
+terminal_app = express();
+
+app.use(uri_root_path + '/terminal', terminal_app);
+
+terminal_app.get('^/?$', function (req, res) {
+    res.redirect(req.baseUrl + '/session/1');
 })
 
-// Setup proxy for the terminal application.
-
-app.use('^' + uri_root_path + '/terminal/', proxy({
-    target: terminal_app,
+terminal_app.use(proxy({
+    target: 'http://127.0.0.1:8081',
     ws: true
 }));
+
+// Start listening for requests.
 
 app.listen(8080);
